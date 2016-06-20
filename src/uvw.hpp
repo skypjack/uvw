7d81366c50ad74fb2d54241b25af3a0b96727475
Michele Caini <michele.caini@gmail.com>
2016-06-20 12:13:50 +0200

    clean up

--- src/uvw.hpp
+++ src/uvw.hpp
@@ -1,6 +1,5 @@
-//#include "uvw/check.hpp"
+#include "uvw/check.hpp"
 #include "uvw/error.hpp"
 #include "uvw/loop.hpp"
 #include "uvw/tcp.hpp"
-//#include "uvw/timer.hpp"
-//#include "uvw/tty.hpp"
+#include "uvw/timer.hpp"
