9adfeb306016da7434bed8548d9490a7b21854c2
Michele Caini <michele.caini@gmail.com>
2017-06-16 12:36:50 +0200

    WIP: tests/coveralls

--- test/uvw/stream.cpp
+++ test/uvw/stream.cpp
@@ -2,6 +2,18 @@
 #include <uvw.hpp>
 
 
+struct fake_stream_t { void *data; };
+struct FakeStreamHandle: uvw::StreamHandle<FakeStreamHandle, fake_stream_t> {
+    using StreamHandle::StreamHandle;
+
+    template<typename... Args>
+    bool init(Args&&...) { return true; }
+};
+
+
 TEST(Stream, TODO) {
+    auto loop = uvw::Loop::getDefault();
+    auto handle = loop->resource<FakeStreamHandle>();
+
     // TODO
 }
