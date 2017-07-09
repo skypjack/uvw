7da264b3f32738768573cb7d21686c9a90b986ce
Michele Caini <michele.caini@gmail.com>
2017-07-09 16:19:29 +0200

    tests, fs_event: rollback, still incomplete

--- test/uvw/fs_event.cpp
+++ test/uvw/fs_event.cpp
@@ -2,40 +2,11 @@
 #include <uvw.hpp>
 
 
-TEST(FsEvent, Functionalities) {
-    const std::string relative = std::string{"test.file"};
-    const std::string absolute = std::string{TARGET_FS_EVENT_DIR} + "/" + relative;
-    const std::string rename = std::string{TARGET_FS_EVENT_DIR} + std::string{"/test.rename"};
-
+TEST(FsEvent, TODO) {
     auto loop = uvw::Loop::getDefault();
-    auto handle = loop->resource<uvw::FsEventHandle>();
-    auto fileReq = loop->resource<uvw::FileReq>();
-    auto fsReq = loop->resource<uvw::FsReq>();
+    auto handle = uvw::FsEventHandle::create(loop);
 
-    bool checkFsEventEvent = false;
+    handle = nullptr;
 
-    handle->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
-    fileReq->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
-    fsReq->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
-
-    handle->on<uvw::FsEventEvent>([&checkFsEventEvent, &relative](const auto &event, auto &hndl) {
-        ASSERT_FALSE(checkFsEventEvent);
-        ASSERT_EQ(std::string{event.filename}, relative);
-        checkFsEventEvent = true;
-        hndl.stop();
-        hndl.close();
-        ASSERT_TRUE(hndl.closing());
-    });
-
-    fileReq->openSync(absolute, O_CREAT | O_RDWR | O_TRUNC, 0644);
-    fsReq->rename(absolute, rename);
-    handle->start(absolute);
-
-    ASSERT_EQ(handle->path(), absolute);
-    ASSERT_TRUE(handle->active());
-    ASSERT_FALSE(handle->closing());
-
-    loop->run();
-
-    ASSERT_TRUE(checkFsEventEvent);
-}
+    // TODO
+ }
