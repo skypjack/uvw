8f3bab832cd22dc14c7ee126b68340324f81ce92
Michele Caini <michele.caini@gmail.com>
2017-07-09 16:20:39 +0200

    Revert "tests, fs_event: rollback, still incomplete"
    This reverts commit 7da264b3f32738768573cb7d21686c9a90b986ce.

--- test/uvw/fs_event.cpp
+++ test/uvw/fs_event.cpp
@@ -2,11 +2,40 @@
 #include <uvw.hpp>
 
 
-TEST(FsEvent, TODO) {
+TEST(FsEvent, Functionalities) {
+    const std::string relative = std::string{"test.file"};
+    const std::string absolute = std::string{TARGET_FS_EVENT_DIR} + "/" + relative;
+    const std::string rename = std::string{TARGET_FS_EVENT_DIR} + std::string{"/test.rename"};
+
     auto loop = uvw::Loop::getDefault();
-    auto handle = uvw::FsEventHandle::create(loop);
+    auto handle = loop->resource<uvw::FsEventHandle>();
+    auto fileReq = loop->resource<uvw::FileReq>();
+    auto fsReq = loop->resource<uvw::FsReq>();
 
-    handle = nullptr;
+    bool checkFsEventEvent = false;
 
-    // TODO
- }
+    handle->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
+    fileReq->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
+    fsReq->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
+
+    handle->on<uvw::FsEventEvent>([&checkFsEventEvent, &relative](const auto &event, auto &hndl) {
+        ASSERT_FALSE(checkFsEventEvent);
+        ASSERT_EQ(std::string{event.filename}, relative);
+        checkFsEventEvent = true;
+        hndl.stop();
+        hndl.close();
+        ASSERT_TRUE(hndl.closing());
+    });
+
+    fileReq->openSync(absolute, O_CREAT | O_RDWR | O_TRUNC, 0644);
+    fsReq->rename(absolute, rename);
+    handle->start(absolute);
+
+    ASSERT_EQ(handle->path(), absolute);
+    ASSERT_TRUE(handle->active());
+    ASSERT_FALSE(handle->closing());
+
+    loop->run();
+
+    ASSERT_TRUE(checkFsEventEvent);
+}
