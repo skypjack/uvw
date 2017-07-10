a852e92e2be7265e3ec08c95449265d05a56918a
Michele Caini <michele.caini@gmail.com>
2017-07-10 23:31:02 +0200

    tests for fs_poll

--- test/uvw/fs_poll.cpp
+++ test/uvw/fs_poll.cpp
@@ -2,11 +2,42 @@
 #include <uvw.hpp>
 
 
-TEST(FsPoll, TODO) {
+TEST(FsPoll, Functionalities) {
+    const std::string filename = std::string{TARGET_FS_POLL_DIR} + std::string{"/test.file"};
+
     auto loop = uvw::Loop::getDefault();
-    auto handle = uvw::FsPollHandle::create(loop);
+    auto handle = loop->resource<uvw::FsPollHandle>();
+    auto request = loop->resource<uvw::FileReq>();
 
-    handle = nullptr;
+    bool checkFsPollEvent = false;
 
-    // TODO
+    handle->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
+    request->on<uvw::ErrorEvent>([](const auto &, auto &) { FAIL(); });
+
+    handle->on<uvw::FsPollEvent>([&checkFsPollEvent](const auto &event, auto &hndl) {
+        ASSERT_FALSE(checkFsPollEvent);
+        checkFsPollEvent = true;
+        hndl.stop();
+        hndl.close();
+        ASSERT_TRUE(hndl.closing());
+    });
+
+    request->on<uvw::FsEvent<uvw::FileReq::Type::WRITE>>([](const auto &, auto &req) {
+        req.close();
+    });
+
+    request->on<uvw::FsEvent<uvw::FileReq::Type::OPEN>>([](const auto &, auto &req) {
+        req.write(std::unique_ptr<char[]>{new char[1]{ 42 }}, 1, 0);
+    });
+
+    handle->start(filename, uvw::FsPollHandle::Time{5});
+    request->open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
+
+    ASSERT_EQ(handle->path(), filename);
+    ASSERT_TRUE(handle->active());
+    ASSERT_FALSE(handle->closing());
+
+    loop->run();
+
+    ASSERT_TRUE(checkFsPollEvent);
 }
