ae6926952ca968f21b6f1a3e66cb84dcf70ad8a6
Michele Caini <michele.caini@gmail.com>
2017-07-12 14:58:11 +0200

    uff... that's all.

--- test/uvw/work.cpp
+++ test/uvw/work.cpp
@@ -37,39 +37,31 @@ TEST(Work, RunTask) {
 }
 
 TEST(Work, Cancellation) {
+    static constexpr int tps = 2;
+    uvw::Utilities::OS::env("UV_THREADPOOL_SIZE", "" + tps);
+
     auto loop = uvw::Loop::getDefault();
     auto handle = loop->resource<uvw::TimerHandle>();
 
     bool checkErrorEvent = false;
-    bool checkWorkEvent = false;
-    bool checkTask = false;
 
     handle->on<uvw::TimerEvent>([](const auto &, auto &hndl) {
         hndl.stop();
         hndl.close();
     });
 
-    auto req = loop->resource<uvw::WorkReq>([&checkTask]() {
-        ASSERT_FALSE(checkTask);
-        checkTask = true;
-    });
+    for(auto i = 0; i < (2 * tps + 1); ++i) {
+        auto req = loop->resource<uvw::WorkReq>([]() { FAIL(); });
 
-    req->on<uvw::ErrorEvent>([&checkErrorEvent](const auto &, auto &) {
-        ASSERT_FALSE(checkErrorEvent);
-        checkErrorEvent = true;
-    });
+        req->on<uvw::WorkEvent>([](const auto &, auto &) { FAIL(); });
+        req->on<uvw::ErrorEvent>([&checkErrorEvent](const auto &, auto &) { checkErrorEvent = true; });
 
-    req->on<uvw::WorkEvent>([&checkWorkEvent](const auto &, auto &) {
-        ASSERT_FALSE(checkWorkEvent);
-        checkWorkEvent = true;
-    });
+        req->queue();
+        req->cancel();
+    }
 
     handle->start(uvw::TimerHandle::Time{500}, uvw::TimerHandle::Time{500});
-    req->queue();
-    req->cancel();
     loop->run();
 
     ASSERT_TRUE(checkErrorEvent);
-    ASSERT_FALSE(checkWorkEvent);
-    ASSERT_FALSE(checkTask);
 }
