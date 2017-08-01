795c6a80324cb326b8114979c35c30ddd74e361a
Tushar Maheshwari <tushar27192@gmail.com>
2017-08-01 20:51:35 +0530

    Common create method (#94)
    * introduce resource_base
    * Fix name resolving for ConstructorAccess
    * Remove cross-ResourceBase friendship
    * Rename to `UnderlyingType`

--- src/uvw/resource.hpp
+++ src/uvw/resource.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
 
-#include <utility>
 #include <memory>
-#include <uv.h>
+#include <utility>
 #include "emitter.hpp"
-#include "loop.hpp"
+#include "underlying_type.hpp"
 
 
 namespace uvw {
@@ -15,48 +14,15 @@ namespace uvw {
  * @brief Common class for almost all the resources available in `uvw`.
  *
  * This is the base class for handles and requests.<br/>
- * It mainly acts as a wrapper around a data structure of `libuv`.
+ * Beyond `UnderlyingType`, it stores a UserData and a self-referencing shared_ptr.
  */
 template<typename T, typename U>
-class Resource: public Emitter<T>, public std::enable_shared_from_this<T> {
-    template<typename, typename>
-    friend class Resource;
+class Resource: public UnderlyingType<T, U>, public Emitter<T>,
+                public std::enable_shared_from_this<T> {
 
 protected:
-    struct ConstructorAccess { explicit ConstructorAccess(int) {} };
-
     auto parent() const noexcept {
-        return pLoop->loop.get();
-    }
-
-    auto get() noexcept {
-        return &resource;
-    }
-
-    auto get() const noexcept {
-        return const_cast<const U *>(&resource);
-    }
-
-    template<typename R>
-    auto get() noexcept {
-        static_assert(not std::is_same<R, U>::value, "!");
-        return reinterpret_cast<R *>(&resource);
-    }
-
-    template<typename R, typename... P>
-    auto get(Resource<P...> &res) noexcept {
-        return reinterpret_cast<R *>(&res.resource);
-    }
-
-    template<typename R>
-    auto get() const noexcept {
-        static_assert(not std::is_same<R, U>::value, "!");
-        return reinterpret_cast<const R *>(&resource);
-    }
-
-    template<typename R, typename... P>
-    auto get(const Resource<P...> &res) const noexcept {
-        return reinterpret_cast<const R *>(&res.resource);
+        return this->loop().loop.get();
     }
 
     void leak() noexcept {
@@ -72,40 +38,14 @@ protected:
     }
 
 public:
-    explicit Resource(ConstructorAccess, std::shared_ptr<Loop> ref)
-        : Emitter<T>{},
-          std::enable_shared_from_this<T>{},
-          pLoop{std::move(ref)},
-          resource{}
+    explicit Resource(typename UnderlyingType<T, U>::ConstructorAccess ca,
+                      std::shared_ptr<Loop> ref)
+        : UnderlyingType<T, U>{std::move(ca), std::move(ref)},
+          Emitter<T>{},
+          std::enable_shared_from_this<T>{}
     {
-        resource.data = static_cast<T*>(this);
+        this->get()->data = static_cast<T*>(this);
     }
-
-    Resource(const Resource &) = delete;
-    Resource(Resource &&) = delete;
-
-    virtual ~Resource() {
-        static_assert(std::is_base_of<Resource<T, U>, T>::value, "!");
-    }
-
-    Resource & operator=(const Resource &) = delete;
-    Resource & operator=(Resource &&) = delete;
-
-    /**
-     * @brief Creates a new resource of the given type.
-     * @param args Arguments to be forwarded to the actual constructor (if any).
-     * @return A pointer to the newly created resource.
-     */
-    template<typename... Args>
-    static std::shared_ptr<T> create(Args&&... args) {
-        return std::make_shared<T>(ConstructorAccess{0}, std::forward<Args>(args)...);
-    }
-
-    /**
-     * @brief Gets the loop from which the resource was originated.
-     * @return A reference to a loop instance.
-     */
-    Loop & loop() const noexcept { return *pLoop; }
 
     /**
      * @brief Gets user-defined data. `uvw` won't use this field in any case.
@@ -127,8 +67,6 @@ public:
 private:
     std::shared_ptr<void> userData{nullptr};
     std::shared_ptr<void> sPtr{nullptr};
-    std::shared_ptr<Loop> pLoop;
-    U resource;
 };
 
 
---
+++ src/uvw/underlying_type.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+#include "loop.hpp"
+
+
+namespace uvw {
+
+
+/**
+ * @brief Internal `class uvw::UnderlyingType`
+ *
+ * A base resource class to define the `create` method in one place.<br/>
+ * It mainly acts as a wrapper around a data structure of `libuv`.
+ */
+template<typename T, typename U>
+class UnderlyingType {
+protected:
+    struct ConstructorAccess { explicit ConstructorAccess(int) {} };
+
+    auto get() noexcept {
+        return &resource;
+    }
+
+    auto get() const noexcept {
+        return &resource;
+    }
+
+    template<typename R>
+    auto get() noexcept {
+        static_assert(not std::is_same<R, U>::value, "!");
+        return reinterpret_cast<R *>(&resource);
+    }
+
+    template<typename R>
+    auto get() const noexcept {
+        static_assert(not std::is_same<R, U>::value, "!");
+        return reinterpret_cast<const R *>(&resource);
+    }
+
+public:
+    explicit UnderlyingType(ConstructorAccess, std::shared_ptr<Loop> ref) noexcept
+        : pLoop{std::move(ref)}, resource{}
+    {}
+
+    UnderlyingType(const UnderlyingType &) = delete;
+    UnderlyingType(UnderlyingType &&) = delete;
+
+    virtual ~UnderlyingType() {
+        static_assert(std::is_base_of<UnderlyingType<T, U>, T>::value, "!");
+    }
+
+    UnderlyingType & operator=(const UnderlyingType &) = delete;
+    UnderlyingType & operator=(UnderlyingType &&) = delete;
+
+    /**
+     * @brief Creates a new resource of the given type.
+     * @param args Arguments to be forwarded to the actual constructor (if any).
+     * @return A pointer to the newly created resource.
+     */
+    template<typename... Args>
+    static std::shared_ptr<T> create(Args&&... args) {
+        return std::make_shared<T>(ConstructorAccess{0}, std::forward<Args>(args)...);
+    }
+
+    /**
+     * @brief Gets the loop from which the resource was originated.
+     * @return A reference to a loop instance.
+     */
+    Loop & loop() const noexcept { return *pLoop; }
+
+private:
+    std::shared_ptr<Loop> pLoop;
+    U resource;
+};
+
+
+}
