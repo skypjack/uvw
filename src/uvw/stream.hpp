e87c2db671d09d97db1a86efecfa57e78e412552
Slyshyk Oleksiy <alexslyshyk@gmail.com>
2017-08-17 12:22:11 +0300

    declare StreamHandle constructor only for MSVC

--- src/uvw/stream.hpp
+++ src/uvw/stream.hpp
@@ -154,7 +154,7 @@ class StreamHandle: public Handle<T, U> {
     }
 
 public:
-#ifdef _WIN32
+#ifdef _MSC_VER
     StreamHandle(typename Handle<T, U>::ConstructorAccess ca, std::shared_ptr<Loop> ref)
         : Handle<T, U>{ca, std::move(ref)}
     {}
