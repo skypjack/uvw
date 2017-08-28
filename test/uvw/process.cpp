01488c71dfdbc75126ad340dadc0f25bc102c70e
Michele Caini <michele.caini@gmail.com>
2017-08-28 23:32:57 +0200

    WIP: tests

--- test/uvw/process.cpp
+++ test/uvw/process.cpp
@@ -2,13 +2,34 @@
 #include <uvw.hpp>
 
 
+TEST(Process, Pid) {
+    auto loop = uvw::Loop::getDefault();
+    auto handle = loop->resource<uvw::ProcessHandle>();
+
+    ASSERT_EQ(handle->pid(), 0);
+
+    loop->run();
+}
+
+
+TEST(Process, Cwd) {
+    auto loop = uvw::Loop::getDefault();
+    auto handle = loop->resource<uvw::ProcessHandle>();
+
+    handle->cwd(".");
+
+    loop->run();
+}
+
+
 TEST(Process, StdIO) {
     auto loop = uvw::Loop::getDefault();
     auto handle = loop->resource<uvw::ProcessHandle>();
     auto pipe = loop->resource<uvw::PipeHandle>();
 
+    uvw::ProcessHandle::disableStdIOInheritance();
     handle->stdio(*pipe, uvw::Flags<uvw::ProcessHandle::StdIO>::from<uvw::ProcessHandle::StdIO::CREATE_PIPE, uvw::ProcessHandle::StdIO::READABLE_PIPE>());
-    handle->stdio(uvw::StdIN, uvw::ProcessHandle::StdIO::READABLE_PIPE);
+    handle->stdio(uvw::StdIN, uvw::ProcessHandle::StdIO::IGNORE_STREAM);
 
     pipe->close();
     loop->run();
