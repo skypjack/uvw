c8c0a44b10eae5ec15fd5555c4309d187b50a078
Michele Caini <michele.caini@gmail.com>
2017-11-29 15:12:10 +0100

    updated doc: v1.5.0

--- search/all_c.js
+++ search/all_c.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['name',['name',['../structuvw_1_1InterfaceAddress.html#abfec6ae9914c03ef90f9d08c0c82759e',1,'uvw::InterfaceAddress::name()'],['../structuvw_1_1ErrorEvent.html#a261276889f9db8882e7412f32808b372',1,'uvw::ErrorEvent::name()']]],
-  ['nameinfo',['nameInfo',['../classuvw_1_1GetNameInfoReq.html#a9318766e0e54deae150ecd8687a32a97',1,'uvw::GetNameInfoReq::nameInfo(std::string ip, unsigned int port, int flags=0)'],['../classuvw_1_1GetNameInfoReq.html#a6771dbaa2015337383ffe0b1a2b0d6af',1,'uvw::GetNameInfoReq::nameInfo(Addr addr, int flags=0)']]],
+  ['nameinfo',['nameInfo',['../classuvw_1_1GetNameInfoReq.html#abf7da3fe9c6fd98cbd8e2d0031f1f3dc',1,'uvw::GetNameInfoReq::nameInfo(const sockaddr &amp;addr, int flags=0)'],['../classuvw_1_1GetNameInfoReq.html#a9318766e0e54deae150ecd8687a32a97',1,'uvw::GetNameInfoReq::nameInfo(std::string ip, unsigned int port, int flags=0)'],['../classuvw_1_1GetNameInfoReq.html#a6771dbaa2015337383ffe0b1a2b0d6af',1,'uvw::GetNameInfoReq::nameInfo(Addr addr, int flags=0)']]],
   ['nameinfoevent',['NameInfoEvent',['../structuvw_1_1NameInfoEvent.html',1,'uvw']]],
-  ['nameinfosync',['nameInfoSync',['../classuvw_1_1GetNameInfoReq.html#a123524dc1c40f11ca6a8763dea75acb1',1,'uvw::GetNameInfoReq::nameInfoSync(std::string ip, unsigned int port, int flags=0)'],['../classuvw_1_1GetNameInfoReq.html#a12b389d3672d88d24c5c41a136e55394',1,'uvw::GetNameInfoReq::nameInfoSync(Addr addr, int flags=0)']]],
+  ['nameinfosync',['nameInfoSync',['../classuvw_1_1GetNameInfoReq.html#aa10d4a54eeef5218cbc0c0f48ca2bbcf',1,'uvw::GetNameInfoReq::nameInfoSync(const sockaddr &amp;addr, int flags=0)'],['../classuvw_1_1GetNameInfoReq.html#a123524dc1c40f11ca6a8763dea75acb1',1,'uvw::GetNameInfoReq::nameInfoSync(std::string ip, unsigned int port, int flags=0)'],['../classuvw_1_1GetNameInfoReq.html#a12b389d3672d88d24c5c41a136e55394',1,'uvw::GetNameInfoReq::nameInfoSync(Addr addr, int flags=0)']]],
   ['netmask',['netmask',['../structuvw_1_1InterfaceAddress.html#ae49c9d7b92ed40cfdf61d846e20eaefa',1,'uvw::InterfaceAddress']]],
   ['nodeaddrinfo',['nodeAddrInfo',['../classuvw_1_1GetAddrInfoReq.html#ad79227261955329b227c04212567779b',1,'uvw::GetAddrInfoReq']]],
   ['nodeaddrinfosync',['nodeAddrInfoSync',['../classuvw_1_1GetAddrInfoReq.html#a2a1dbef8fa0cb180ab87e008958334da',1,'uvw::GetAddrInfoReq']]],
