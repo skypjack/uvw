cd921370e1b8bf4625f3697a194a1c2f6e04efc0
Michele Caini <michele.caini@gmail.com>
2017-12-03 15:34:43 +0100

    updated doc: v1.6.0

--- search/functions_e.js
+++ search/functions_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['parent',['parent',['../structuvw_1_1Utilities_1_1OS.html#a2145593521219f0184522ea6b7b6d790',1,'uvw::Utilities::OS']]],
   ['passwd',['passwd',['../structuvw_1_1Utilities_1_1OS.html#a9d2504b52c450da8620741a835eae70b',1,'uvw::Utilities::OS']]],
   ['path',['path',['../classuvw_1_1FsEventHandle.html#adc8fc25a6daa924b1097f43ccda5db3c',1,'uvw::FsEventHandle::path()'],['../classuvw_1_1FsPollHandle.html#a9baac78743b8cfe9bf581c20e2255c4f',1,'uvw::FsPollHandle::path()'],['../structuvw_1_1Utilities.html#a93c08c9616de565c4b34d7f580f94fb8',1,'uvw::Utilities::path()']]],
   ['peer',['peer',['../classuvw_1_1PipeHandle.html#af15f10d5b58ef480a6c33e0b0f5387e8',1,'uvw::PipeHandle::peer()'],['../classuvw_1_1TcpHandle.html#a0d1a767f0f6da7f32e492485f4adab1a',1,'uvw::TcpHandle::peer()']]],
