d35806cee71379c0058f5695610a72769050793f
Michele Caini <michele.caini@gmail.com>
2018-01-19 12:12:45 +0100

    updated doc: v1.7.0

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['data',['data',['../classuvw_1_1Loop.html#a07480967d95c0f9908fc75292b07c858',1,'uvw::Loop::data() const'],['../classuvw_1_1Loop.html#a8ab9a0c0d23b615c3f5de7b15d742aaf',1,'uvw::Loop::data(std::shared_ptr&lt; void &gt; uData)']]],
   ['datasync',['datasync',['../classuvw_1_1FileReq.html#a23bc385a7ebdb6eb149b3311cb630ab3',1,'uvw::FileReq']]],
   ['datasyncsync',['datasyncSync',['../classuvw_1_1FileReq.html#a99c13abb2aae436a94ce1d83affb7a6b',1,'uvw::FileReq']]],
   ['descriptor',['descriptor',['../classuvw_1_1Loop.html#ac78ffc161590ea88902c867ef716d0a2',1,'uvw::Loop']]],
