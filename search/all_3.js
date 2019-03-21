e33405aee7e00ade576b0c189fdd0ca64d8b95c5
Michele Caini <michele.caini@gmail.com>
2019-03-21 14:15:29 +0100

    updated doc: v1.15.0

--- search/all_3.js
+++ search/all_3.js
@@ -5,5 +5,6 @@ var searchData=
   ['datasync',['datasync',['../classuvw_1_1FileReq.html#a23bc385a7ebdb6eb149b3311cb630ab3',1,'uvw::FileReq']]],
   ['datasyncsync',['datasyncSync',['../classuvw_1_1FileReq.html#a99c13abb2aae436a94ce1d83affb7a6b',1,'uvw::FileReq']]],
   ['descriptor',['descriptor',['../classuvw_1_1Loop.html#ac78ffc161590ea88902c867ef716d0a2',1,'uvw::Loop']]],
-  ['disablestdioinheritance',['disableStdIOInheritance',['../classuvw_1_1ProcessHandle.html#a99d3c33bcc1d564080874c3378e5a39d',1,'uvw::ProcessHandle']]]
+  ['disablestdioinheritance',['disableStdIOInheritance',['../classuvw_1_1ProcessHandle.html#a99d3c33bcc1d564080874c3378e5a39d',1,'uvw::ProcessHandle']]],
+  ['disconnect',['disconnect',['../classuvw_1_1UDPHandle.html#aae0a9ec23746b395ec91b5e62b6b95d5',1,'uvw::UDPHandle']]]
 ];
