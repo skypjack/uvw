2743f16d6cbe60bf4dc485200644f983138895d1
Michele Caini <michele.caini@gmail.com>
2022-04-08 08:41:38 +0200

    oops

--- test/uvw/request.cpp
+++ test/uvw/request.cpp
@@ -7,7 +7,7 @@ TEST(Request, Functionalities) {
     auto req = loop->resource<uvw::work_req>([]() {});
 
     ASSERT_NE(req->size(), decltype(req->size()){0});
-    ASSERT_EQ(0, req->cancel());
+    ASSERT_LT(req->cancel(), 0);
 
     loop->run();
 }
