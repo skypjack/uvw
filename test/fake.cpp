68c55389a6a7859a8035bb8cd657b452f2b80d95
skypjack <michele.caini@gmail.com>
2025-11-26 16:32:30 +0100

    test: clang-tidy related changes

--- test/fake.cpp
+++ test/fake.cpp
@@ -1,6 +1,6 @@
 extern "C" int fake_func(double *);
 
-int fake_func(double *d) {
-    if(d) { *d = -*d; }
-    return -42;
+int fake_func(double *value) {
+    if(value != nullptr) { *value = -*value; }
+    return -1;
 }
